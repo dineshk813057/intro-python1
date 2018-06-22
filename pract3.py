67691194c65636afca9ac8b49ee22fa1a303abed
dineshk813057 <40352058+dineshk813057@users.noreply.github.com>
2018-06-22 11:01:58 +0530

    Add files via upload

---
+++ pract3.py
@@ -0,0 +1,25 @@
+# question 1
+print("first program")
+
+#question 2
+print("acad"+"view")
+
+#question 3
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+z=int(input("enter third number"))
+print("first number"+str(x),"second number"+str(y),"third number"+str(x))
+
+#question4
+print("let's get started")
+
+#question5
+s="acadview"
+course="python"
+fees=5000
+print("name of institute is %s and course name is %s and fees is %d" %(s,course,fees))
+
+#question 6
+name="tony stark"
+salary=100000
+print(('%s  %d')%(name,salary))
